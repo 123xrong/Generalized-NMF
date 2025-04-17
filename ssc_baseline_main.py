9f6dfe3889929369aaf21eeb99afed9fa6a93326
123xrong <xrong8@wisc.edu>
2025-04-16 20:17:15 -0500

    updated ssc baseline

--- ssc_baseline_main.py
+++ ssc_baseline_main.py
@@ -26,7 +26,7 @@ def main(m, r, n_k, K, sigma=0.0, alpha = 0.01, random_state=None, max_iter=50, 
 
     # Simulate your data matrix X of shape (m, n_k * K)
     X, true_labels = data_simulation(m, r, n_k, K, sigma, random_state)  # Make sure this is defined
-    pre_labels, accuracy = baseline_ssc(X, alpha, max_iter=max_iter, tol=tol, random_state=random_state)
+    pre_labels, accuracy = baseline_ssc(X, true_labels = true_labels, alpha=alpha)
 
     wandb.log({
         "accuracy": accuracy,
