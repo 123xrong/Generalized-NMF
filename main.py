85d21cbf85dc6d9721600e89785c22b3bdb2c4f8
123xrong <xrong8@wisc.edu>
2025-04-18 22:41:59 -0500

    updated iterative cc

--- main.py
+++ main.py
@@ -14,14 +14,13 @@ def arg_parser():
     parser.add_argument('--K', type=int, default=4, help='Number of subspaces (default: 3)')
     parser.add_argument('--sigma', type=float, default=0.0, help='Standard deviation of Gaussian noise (default: 0.0)')
     parser.add_argument('--max_iter', type=int, default=200, help='Maximum number of iterations (default: 50)')
-    parser.add_argument('--tol', type=float, default=1e-6, help='Tolerance for stopping criterion (default: 1e-6)')
     parser.add_argument('--random_state', type=int, default=42, help='Random seed for clustering (default: None)')
     parser.add_argument('--NMF_method', choices=['anls', 'NMF'], default='anls', help='NMF method to use')
     parser.add_argument('--NMF_solver', choices=['cd', 'mu'], default='cd', help='Solver for NMF')
     return parser.parse_args()
 
 
-def main(m, r, n_k, K, sigma=0.0, random_state=None, max_iter=50, tol=1e-6):
+def main(m, r, n_k, K, sigma=0.0, random_state=None, max_iter=50):
 
     wandb.init(
         project="coneClustering",
@@ -31,19 +30,19 @@ def main(m, r, n_k, K, sigma=0.0, random_state=None, max_iter=50, tol=1e-6):
     X, true_labels = data_simulation(m, r, n_k, K, sigma=sigma, random_state=random_state)
 
     # 2. Run iterative subspace clustering
-    X_new, cluster_labels, errors, accuracy, neg_prop, iterations, loss = coneClus_iterative(
-        X, K, r, true_labels, max_iter=max_iter, tol=tol, random_state=random_state
+    accuracy, reconstruction_error, neg_prop= coneClus_iterative(
+        X, K, r, true_labels, max_iter=max_iter, random_state=random_state
     )
 
     # 3. Log results
     wandb.log({
         "accuracy": accuracy,
-        "reconstruction_error": loss
+        "reconstruction_error": reconstruction_error
     })
 
     print("\n--- Results ---")
     print(f"Clustering Accuracy (ARI): {accuracy:.4f}")
-    print(f"Final Reconstruction Loss: {loss:.4f}")
+    print(f"Final Reconstruction Loss: {reconstruction_error:.4f}")
 
 if __name__ == "__main__":
     args = arg_parser()
@@ -54,5 +53,4 @@ if __name__ == "__main__":
     sigma = args.sigma
     random_state = args.random_state
     max_iter = args.max_iter
-    tol = args.tol
-    main(m, r, n_k, K, sigma=sigma, random_state=random_state, max_iter=max_iter, tol=tol)
+    main(m, r, n_k, K, sigma=sigma, random_state=random_state, max_iter=max_iter)
