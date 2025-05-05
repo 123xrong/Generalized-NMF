9396a40989445b2ad69408f3bc0801cb7a87b9c1
123xrong <xrong8@wisc.edu>
2025-05-05 14:28:15 -0500

    Update sscnmf_MNIST_main.py

--- sscnmf_MNIST_main.py
+++ sscnmf_MNIST_main.py
@@ -16,7 +16,7 @@ labels = []
 for digit in range(4):
     idx = np.where(y_full == digit)[0]
     selected_idx = np.random.choice(idx, 50, replace=False)
-    X_list.append(X_full[idx])
+    X_list.append(X_full[selected_idx])
     labels.append(np.full(len(selected_idx), digit))
 
 X_subset = np.vstack(X_list)
