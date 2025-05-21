3ba928433b9142181f5867ffd96ee2bda9180817
123xrong <xrong8@wisc.edu>
2025-05-21 14:34:37 -0700

    Update RICC_realdata_main.py

--- RICC_realdata_main.py
+++ RICC_realdata_main.py
@@ -9,7 +9,7 @@ def arg_parser():
     # parser.add_argument('--m', type=int, default=50, help='Dimension of the ambient space (default: 50)')
     parser.add_argument('--r', type=int, default=5, help='Dimension (rank) of each subspace (default: 5)')
     parser.add_argument('--n', type=int, default=50, help='Number of points per subspace (default: 100)')
-    parser.add_argument('--K', type=int, default=4, help='Number of subspaces (default: 3)')
+    parser.add_argument('--K', type=int, default=5, help='Number of subspaces (default: 3)')
     parser.add_argument('--sigma', type=float, default=0.0, help='Standard deviation of Gaussian noise (default: 0.0)')
     parser.add_argument('--max_iter', type=int, default=200, help='Maximum number of iterations (default: 50)')
     parser.add_argument('--random_state', type=int, default=42, help='Random seed for clustering (default: None)')
@@ -20,17 +20,22 @@ def arg_parser():
     parser.add_argument('--dataset', choices=['mnist', 'YaleB', 'ORL'], default='mnist', help='Dataset to use (default: mnist)')
     return parser.parse_args()
 
-def load_dataset(name):
+def load_dataset(name, n, K):
     if name == 'mnist':
         print("Loading MNIST...")
         mnist = fetch_openml('mnist_784', version=1, as_frame=False)
         X_full = mnist.data / 255.0  # normalize to [0,1]
         y_full = mnist.target.astype(int)
 
-        # Subset: select digits 0–5 for faster experiments
-        mask = y_full < 6
-        X = X_full[mask].T  # shape (784, n)
-        y = y_full[mask]
+        X_list = []
+        labels = []
+        for digit in range(K+1):
+            idx = np.where(y_full == digit)[0]
+            selected_idx = np.random.choice(idx, n, replace=False)
+            X_list.append(X_full[selected_idx])
+            labels.append(np.full(len(selected_idx), digit))
+        X_full = np.vstack(X_list)
+        y_full = np.concatenate(labels)
 
     elif name == 'ORL':
         print("Loading ORL (Olivetti Faces)...")
@@ -39,7 +44,7 @@ def load_dataset(name):
         y_full = faces.target
 
         # Subset: select only classes 0–5 (6 people × 10 = 60 samples)
-        mask = y_full < 6
+        mask = y_full < K+1
         X = faces.data[mask].T  # shape (4096, 60)
         y = y_full[mask]
 
@@ -50,7 +55,7 @@ def load_dataset(name):
     return X, y
 
 def main(r, K, n, NMF_method='anls', sigma=0.0, random_state=None, max_iter=200, alpha=0.5, ord=2, dataset='mnist'):
-    X_subset, true_labels = load_dataset(dataset)
+    X_subset, true_labels = load_dataset(dataset, n, K)
 
     X_subset = X_subset.T
     X_preprocessed = X_subset / 255.0  # normalize to [0, 1]
