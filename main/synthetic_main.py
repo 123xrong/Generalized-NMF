e1ca67921205d7def9722c79b6f768a555d9f6a1
123xrong <xrong8@wisc.edu>
2025-07-22 22:37:02 -0700

    Update synthetic_main.py

--- main/synthetic_main.py
+++ main/synthetic_main.py
@@ -30,7 +30,7 @@ def arg_parser():
     parser.add_argument('--NMF_method', type=str, default='anls', choices=['mu', 'cd', 'anls'], help='NMF solver method (default: anls)')
     return parser.parse_args()
 
-def main(model, m, r, n, K, sigma=0.0, alpha=0.1, l1_reg=0.01, random_state=42, max_iter=50, tol=1e-6, NMF_method='anls'):
+def main(model, m, r, n, K, sigma=0.0, alpha=0.01, l1_reg=0.01, random_state=42, max_iter=50, tol=1e-6, NMF_method='anls'):
     X, true_labels = data_simulation(m, r, n, K, sigma=sigma, random_state=random_state)
     print(f"Received model: {model}")
     if model == 'sscnmf':
