4968a328540a47a1080c3b1b0dafde75f3a63fd6
123xrong <xrong8@wisc.edu>
2025-07-28 11:32:32 -0700

    Update WebKB_main.py

--- main/WebKB_main.py
+++ main/WebKB_main.py
@@ -35,7 +35,7 @@ def arg_parser():
 
 def main(model, r, n, K, sigma=0.0, alpha=0.1, l1_reg=0.01, random_state=42, max_iter=50, tol=1e-6):
     coil20_data = loadmat('data/PAGE.mat')
-    X_full = coil20_data['X'].T  # shape (feature_dim, num_samples)
+    X = coil20_data['X'].T  # shape (feature_dim, num_samples)
     true_labels = coil20_data['Y'].flatten() - 1  # Convert
 
     # X = normalize(X_full, axis=0)
