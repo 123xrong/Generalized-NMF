ec553b1a0f102864b05969c39ca7aa816ed44db9
123xrong <xrong8@wisc.edu>
2025-07-29 12:24:54 -0700

    created deep SSCNMF

--- main/MNIST_main.py
+++ main/MNIST_main.py
@@ -14,6 +14,7 @@ from coneClustering import *
 from modified_dscnmf import *
 from baseline import *
 from deepNMF import *
+from deepSSCNMF import *
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import normalize
 from nmf import *
@@ -30,7 +31,7 @@ def arg_parser():
     parser.add_argument('--max_iter', type=int, default=200, help='Maximum number of iterations (default: 50)')
     parser.add_argument('--tol', type=float, default=1e-6, help='Tolerance for stopping criterion (default: 1e-6)')
     parser.add_argument('--random_state', type=int, default=42, help='Random seed for clustering (default: None)')
-    parser.add_argument('--model', type=str, choices=['sscnmf', 'ricc', 'gnmf', 'gpcanmf', 'onmf_relu', 'dscnmf', 'onmf', 'deepnmf'],
+    parser.add_argument('--model', type=str, choices=['sscnmf', 'ricc', 'gnmf', 'gpcanmf', 'onmf_relu', 'dscnmf', 'onmf', 'deepnmf', 'deepsscnmf'],
     help='Model to use for clustering')
     parser.add_argument('--l1_reg', type=float, default=0.01, help='L1 regularization parameter for ONMF-ReLU/GPCANMF')
     return parser.parse_args()
@@ -82,6 +83,8 @@ def main(model, r, n, K, sigma=0.0, alpha = 0.1, l1_reg=0.01, random_state=42, m
         project_name = 'onmf-MNIST'
     elif model == 'deepnmf':
         project_name = 'deepnmf-MNIST'
+    elif model == 'deepsscnmf':
+        project_name = 'deepsscnmf-MNIST'
 
     wandb.init(
         project="coneClustering",
@@ -113,6 +116,10 @@ def main(model, r, n, K, sigma=0.0, alpha = 0.1, l1_reg=0.01, random_state=42, m
     elif model == 'deepnmf':    
         acc, ARI, NMI, reconstruction_error = deep_nmf(
             X_subset, true_labels=true_labels)
+    elif model == 'deepsscnmf':
+        acc, ARI, NMI, reconstruction_error = deep_ssc_nmf(
+            X_subset, ranks=[256, 128, 64], alpha=alpha, n_iter=max_iter,
+            true_labels=true_labels)
 
     wandb.log({
         "accuracy": acc,
