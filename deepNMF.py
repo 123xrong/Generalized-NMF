05c6a87b9905aa41d9af47febcbefa4d012153fa
123xrong <xrong8@wisc.edu>
2025-07-29 12:00:23 -0700

    Update deepNMF.py

--- deepNMF.py
+++ deepNMF.py
@@ -36,7 +36,7 @@ class DeepNMF(nn.Module):
 
 def deep_nmf(X_np, r1=256, r2=128, r3=64, n_iter=200, true_labels=None, device='cpu'):
     # Normalize and convert to torch tensor
-    X_np = normalize(X_np, axis=0)
+    X_np = normalize(X_np, axis=1)
     X = torch.tensor(X_np, dtype=torch.float32).to(device)
     norm_X = torch.norm(X, p='fro')
 
