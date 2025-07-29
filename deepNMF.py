da8f5983b93013b71bb576002a026e3cd493af4a
123xrong <xrong8@wisc.edu>
2025-07-29 11:12:48 -0700

    Update deepNMF.py

--- deepNMF.py
+++ deepNMF.py
@@ -71,5 +71,4 @@ def deep_nmf(X_np, r1=256, r2=128, r3=64, n_iter=200, true_labels=None, device='
         'ARI': ari,
         'NMI': nmi,
         'reconstruction_error': recon_error,
-        'H': H_final
     }
