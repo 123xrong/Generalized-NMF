61ea1cb1f5ab319b94504006245fb11be8ce2d31
123xrong <xrong8@wisc.edu>
2025-10-16 18:01:19 -0500

    updated WebKB main

--- main/WebKB_main.py
+++ main/WebKB_main.py
@@ -30,14 +30,15 @@ def arg_parser():
     parser.add_argument('--max_iter', type=int, default=200, help='Maximum number of iterations (default: 50)')
     parser.add_argument('--tol', type=float, default=1e-6, help='Tolerance for stopping criterion (default: 1e-6)')
     parser.add_argument('--random_state', type=int, default=None, help='Random seed for clustering (default: None)')
-    parser.add_argument('--model', type=str, choices=['sscnmf', 'ricc', 'gnmf', 'gpcanmf', 'onmf_relu', 'dscnmf', 'onmf', 'deepnmf', 'deepsscnmf'],
+    parser.add_argument('--model', type=str, choices=['sscnmf', 'ricc', 'gnmf', 'gpcanmf', 'onmf_relu', 'dscnmf', 'onmf', 'deepnmf', 'deepsscnmf', 'ssc-omp-nmf'],
                         help='Model to use for clustering')
     parser.add_argument('--graph_weighting', type=str, default='dot-product', choices=['heat-kernel', 'binary', 'dot-product'],)
     parser.add_argument('--l1_reg', type=float, default=0.01,
                         help='L1 regularization parameter for ONMF-ReLU/GPCANMF')
+    parser.add_argument('--n_nonzero_coefs', type=int, default=8, help='Number of non-zero coefficients for OMP (default: 8)')
     return parser.parse_args()
 
-def main(model, r, n, K, sigma=0.0, alpha=0.1, l1_reg=0.01, random_state=None, max_iter=50, tol=1e-6, graph_weighting='dot-product'):
+def main(model, r, n, K, sigma=0.0, alpha=0.1, l1_reg=0.01, random_state=None, max_iter=50, tol=1e-6, graph_weighting='dot-product', n_nonzero_coefs=8):
     dataset = WebKB(root='~/data/WebKB', name='Cornell')
     data = dataset[0]
     X = data.x.numpy().T  # feature matrix: (features, samples)
@@ -53,8 +54,12 @@ def main(model, r, n, K, sigma=0.0, alpha=0.1, l1_reg=0.01, random_state=None, m
     
     if model == 'sscnmf':
         project_name = 'sscnmf-WEBKB'
-        acc, ARI, NMI, reconstruction_error = ssc_nmf_baseline(
+        acc, ARI, NMI, reconstruction_error, _, _, _ = ssc_nmf_baseline(
             X, K=K, r=r, true_labels=true_labels, alpha=alpha)
+    elif model == 'ssc-omp-nmf':
+        project_name = 'ssc-omp-nmf-WEBKB'
+        acc, ARI, NMI, reconstruction_error, _, _, _ = ssc_omp_nmf_baseline(
+            X, K=K, r=r, true_labels=true_labels, n_nonzero_coefs=n_nonzero_coefs, random_state=random_state)
     elif model == 'ricc':
         project_name = 'ricc-WEBKB'
         acc, ARI, NMI, reconstruction_error, _, _, _ = iter_reg_coneclus_warmstart(
@@ -118,5 +123,6 @@ if __name__ == "__main__":
     tol = args.tol
     random_state = args.random_state
     graph_weighting = args.graph_weighting
+    n_nonzero_coefs = args.n_nonzero_coefs
 
-    main(model, r, n, K, sigma, alpha, l1_reg, random_state, max_iter, tol, graph_weighting)
+    main(model, r, n, K, sigma, alpha, l1_reg, random_state, max_iter, tol, graph_weighting, n_nonzero_coefs)
